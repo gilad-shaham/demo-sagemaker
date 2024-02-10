28bd43d8d410d47630d258488d82b64d4dc27cb3
root <gilads@iguazio.com>
2024-02-10 17:25:46 +0000

    test without env file

--- project_setup.py
+++ project_setup.py
@@ -57,9 +57,10 @@ def setup(
 
 def _set_secrets(project: mlrun.projects.MlrunProject):
     # Set the secrets:
-    project.set_secrets(file_path="mlrun.env")
+#    project.set_secrets(file_path="mlrun.env")
     # Set as environment variables:
-    mlrun.set_env_from_file("mlrun.env")
+#    mlrun.set_env_from_file("mlrun.env")
+    pass
 
 
 def _build_image(project: mlrun.projects.MlrunProject):
